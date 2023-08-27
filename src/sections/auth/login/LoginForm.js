20820baca1807e81b29cb87c1915fca778303db9
MajorTomidev <majortomiodukoya@gmail.com>
2023-08-27 22:21:43 +0100

    upload

--- src/sections/auth/login/LoginForm.js
+++ src/sections/auth/login/LoginForm.js
@@ -39,7 +39,7 @@ export default function LoginForm() {
       </Stack>
 
       <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
-        <Checkbox name="remember" label="Remember me" />
+        <Stack direction="row" sx={{ my: 2 }}><Checkbox name="remember" label="Remember me" /> <p>Remember me</p></Stack>
         <Link variant="subtitle2" underline="hover">
           Forgot password?
         </Link>
